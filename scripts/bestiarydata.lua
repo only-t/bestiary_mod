82b87b08f173cdb97653b2e2584b9dd3094d1c95
T <onlyt.coding@gmail.com>
2022-03-07 15:23:47 +0100

    Removed self.new_mobs and replaced it with the IsNew function. Removed RemoveFromTable function.

--- scripts/bestiarydata.lua
+++ scripts/bestiarydata.lua
@@ -2,12 +2,11 @@ local BestiaryData = Class(function(self)
 	self.discovered_mobs = {  }
 	self.learned_mobs = {  }
 
-	-- self.new_mobs = {  }
+	self.new_mobs = {  }
 end)
 
 function BestiaryData:Save(force_save)
 	if force_save then
-		-- local str = json.encode({ discovered_mobs = self.discovered_mobs, learned_mobs = self.learned_mobs, new_mobs = self.new_mobs })
 		local str = json.encode({ discovered_mobs = self.discovered_mobs, learned_mobs = self.learned_mobs })
 
 		TheSim:SetPersistentString("bestiary", str, false) -- Basically a carbon copy of cookbooks saving/loading
@@ -25,7 +24,6 @@ function BestiaryData:Load()
 		    if status and bestiary then
 				self.discovered_mobs = bestiary.discovered_mobs or {  }
 				self.learned_mobs = bestiary.learned_mobs or {  }
-				-- self.new_mobs = bestiary.new_mobs or {  }
             else
                 print("Failed to load the bestiary!", status, bestiary)
             end
@@ -52,7 +50,7 @@ function BestiaryData:DiscoverMob(prefab)
 
     if ValidDiscoverableMob(prefab) then
         table.insert(self.discovered_mobs, prefab)
-        -- table.insert(self.new_mobs, prefab)
+        self.new_mobs[prefab] = true
 
         if ThePlayer then -- Push only it's the client
 		    ThePlayer:PushEvent("mob_discovered")
@@ -94,20 +92,14 @@ function BestiaryData:LearnMob(prefab)
     end
 end
 
-function BestiaryData:RemoveFromTable(table, value)
-    for i, table_value in ipairs(self[table]) do
-		if table_value == value then
-			self[table][i] = nil
-		end
-	end
-
-    self:Save(true)
+function BestiaryData:IsNew(value)
+    return self.new_mobs[value]
 end
 
 function BestiaryData:Forgor()
     self.discovered_mobs = {  }
 	self.learned_mobs = {  }
-    -- self.new_mobs = {  }
+    self.new_mobs = {  }
 
     self:Save(true)
 end
