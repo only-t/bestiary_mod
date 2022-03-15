8423bdbf43e8dabff2543e4d842bcfebe9647cbb
T <onlyt.coding@gmail.com>
2022-03-15 18:39:47 +0100

    Added a crafting index for the bestiary.

--- init/init_recipes.lua
+++ init/init_recipes.lua
@@ -2,4 +2,6 @@ local Ingredient = GLOBAL.Ingredient
 local TECH = GLOBAL.TECH
 local CRAFTING_FILTERS = GLOBAL.CRAFTING_FILTERS
 
-AddRecipe2("bestiary", { Ingredient("papyrus", 1), Ingredient("monstermeat", 1) }, TECH.SCIENCE_ONE, { atlas = "images/bestiary.xml", image = "bestiary.tex" })
+AddRecipe2("bestiary", { Ingredient("papyrus", 1), Ingredient("monstermeat", 1) }, TECH.SCIENCE_ONE, { atlas = "images/bestiary.xml", image = "bestiary.tex" }, { "TOOLS" })
+table.remove(CRAFTING_FILTERS.TOOLS.recipes)
+table.insert(CRAFTING_FILTERS.TOOLS.recipes, 15.5, "bestiary")
