6d6b1c35d8467d3d8901722f5f31e2301efdb594
T <onlyt.coding@gmail.com>
2022-05-27 21:12:34 +0200

    Moved setting up discoverable_prefabs to a separate file.

---
+++ scripts/discoverable_prefabs.lua
@@ -0,0 +1,159 @@
+AddPrefabPostInit("killerbee", function(inst)
+	if not GLOBAL.TheWorld.ismastersim then
+		return inst
+	end
+
+	inst.discoverable_prefab = "bee"
+end)
+
+AddPrefabPostInit("beeguard", function(inst)
+	if not GLOBAL.TheWorld.ismastersim then
+		return inst
+	end
+
+	inst.discoverable_prefab = "bee"
+end)
+
+AddPrefabPostInit("canary_poisoned", function(inst)
+	if not GLOBAL.TheWorld.ismastersim then
+		return inst
+	end
+
+	inst.discoverable_prefab = "canary"
+end)
+
+AddPrefabPostInit("crawlingnightmare", function(inst)
+	if not GLOBAL.TheWorld.ismastersim then
+		return inst
+	end
+
+	inst.discoverable_prefab = "crawlinghorror"
+end)
+
+AddPrefabPostInit("nightmarebeak", function(inst)
+	if not GLOBAL.TheWorld.ismastersim then
+		return inst
+	end
+
+	inst.discoverable_prefab = "terrorbeak"
+end)
+
+AddPrefabPostInit("bishop_nightmare", function(inst)
+	if not GLOBAL.TheWorld.ismastersim then
+		return inst
+	end
+
+	inst.discoverable_prefab = "bishop"
+end)
+
+AddPrefabPostInit("knight_nightmare", function(inst)
+	if not GLOBAL.TheWorld.ismastersim then
+		return inst
+	end
+
+	inst.discoverable_prefab = "knight"
+end)
+
+AddPrefabPostInit("rook_nightmare", function(inst)
+	if not GLOBAL.TheWorld.ismastersim then
+		return inst
+	end
+
+	inst.discoverable_prefab = "rook"
+end)
+
+AddPrefabPostInit("firehound", function(inst)
+	if not GLOBAL.TheWorld.ismastersim then
+		return inst
+	end
+
+	inst.discoverable_prefab = "hound"
+end)
+
+AddPrefabPostInit("icehound", function(inst)
+	if not GLOBAL.TheWorld.ismastersim then
+		return inst
+	end
+
+	inst.discoverable_prefab = "hound"
+end)
+
+AddPrefabPostInit("koalefant_winter", function(inst)
+	if not GLOBAL.TheWorld.ismastersim then
+		return inst
+	end
+
+	inst.discoverable_prefab = "koalefant_summer"
+end)
+
+AddPrefabPostInit("snurtle", function(inst)
+	if not GLOBAL.TheWorld.ismastersim then
+		return inst
+	end
+
+	inst.discoverable_prefab = "slurtle"
+end)
+
+AddPrefabPostInit("toadstool_dark", function(inst)
+	if not GLOBAL.TheWorld.ismastersim then
+		return inst
+	end
+
+	inst.discoverable_prefab = "toadstool"
+end)
+
+AddPrefabPostInit("wobybig", function(inst)
+	if not GLOBAL.TheWorld.ismastersim then
+		return inst
+	end
+
+	inst.discoverable_prefab = "wobysmall"
+end)
+
+AddPrefabPostInit("alterguardian_phase2", function(inst)
+	if not GLOBAL.TheWorld.ismastersim then
+		return inst
+	end
+
+	inst.discoverable_prefab = "alterguardian_phase1"
+end)
+
+AddPrefabPostInit("alterguardian_phase3", function(inst)
+	if not GLOBAL.TheWorld.ismastersim then
+		return inst
+	end
+
+	inst.discoverable_prefab = "alterguardian_phase1"
+end)
+
+AddPrefabPostInit("stalker_minion2", function(inst)
+	if not GLOBAL.TheWorld.ismastersim then
+		return inst
+	end
+
+	inst.discoverable_prefab = "stalker_minion1"
+end)
+
+AddPrefabPostInit("leif_sparse", function(inst)
+	if not GLOBAL.TheWorld.ismastersim then
+		return inst
+	end
+
+	inst.discoverable_prefab = "leif"
+end)
+
+AddPrefabPostInit("wobster_sheller", function(inst)
+	if not GLOBAL.TheWorld.ismastersim then
+		return inst
+	end
+
+	inst.discoverable_prefab = "wobster_sheller_land"
+end)
+
+AddPrefabPostInit("wobster_moonglass", function(inst)
+	if not GLOBAL.TheWorld.ismastersim then
+		return inst
+	end
+
+	inst.discoverable_prefab = "wobster_moonglass_land"
+end)
